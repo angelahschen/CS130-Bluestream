738c96672a95b3b7965fe0b5b4814cabcf82ec70
Sahil Shridhar <sahil95@gmail.com>
2017-04-25 23:17:34 -0700

    created basic dashboard
    refer to dashboard.html for first page

---
+++ dashboard/assets/js/dashboardjs.js
@@ -0,0 +1,108 @@
+function newElement() {
+  //alert("I am an alert box!");
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+$("#sortable").sortable();
+$("#sortable").disableSelection();
+
+countTodos();
+
+// all done btn
+$("#checkAll").click(function(){
+    AllDone();
+});
+
+//create todo
+$('.add-todo').on('keypress',function (e) {
+      e.preventDefault
+      if (e.which == 13) {
+           if($(this).val() != ''){
+           var todo = $(this).val();
+            createTodo(todo); 
+            countTodos();
+           }else{
+               // some validation
+           }
+      }
+});
+// mark task as done
+$('.todolist').on('change','#sortable li input[type="checkbox"]',function(){
+    if($(this).prop('checked')){
+        var doneItem = $(this).parent().parent().find('label').text();
+        $(this).parent().parent().parent().addClass('remove');
+        done(doneItem);
+        countTodos();
+    }
+});
+
+//delete done task from "already done"
+$('.todolist').on('click','.remove-item',function(){
+    removeItem(this);
+});
+
+// count tasks
+function countTodos(){
+    var count = $("#sortable li").length;
+    $('.count-todos').html(count);
+}
+
+//create task
+function createTodo(text){
+    var markup = '<li class="ui-state-default"><div class="checkbox"><label><input type="checkbox" value="" />'+ text +'</label></div></li>';
+    $('#sortable').append(markup);
+    $('.add-todo').val('');
+}
+
+//mark task as done
+function done(doneItem){
+    var done = doneItem;
+    var markup = '<li>'+ done +'<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>';
+    $('#done-items').append(markup);
+    $('.remove').remove();
+}
+
+//mark all tasks as done
+function AllDone(){
+    var myArray = [];
+
+    $('#sortable li').each( function() {
+         myArray.push($(this).text());   
+    });
+    
+    // add to done
+    for (i = 0; i < myArray.length; i++) {
+        $('#done-items').append('<li>' + myArray[i] + '<button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>');
+    }
+    
+    // myArray
+    $('#sortable li').remove();
+    countTodos();
+}
+
+//remove done task from list
+function removeItem(element){
+    $(element).parent().remove();
+}
