3d8fcadb5ab0e2397c0beb0298ce4cde1e226a48
Fan Hung <fhung65@gmail.com>
2017-06-03 19:47:29 -0700

    commit message:
    added saving section data for section 4 and 5
    	added tracking id of project being worked on to request.session
    	added a get request param at projects list to set this id
    took out class based views
    	also took out a todo to use mixins for auth
    started docs folder
    	containing a start to a code style text file
    	did some code cleanup to match style text

--- Bluestream/site_ver1/views.py
+++ Bluestream/site_ver1/views.py
@@ -15,150 +15,161 @@ from django.contrib.auth import authenticate, login, logout
 from django.core.mail import EmailMessage
 # Create your views here.
 
-class HomePageView(TemplateView):
-    template_name = "index.html"
+def homepage(request):
+    return render(request, 'index.html')
 
-class LoginPageView(TemplateView):
-    template_name = "loginpage.html"
-
-class RegisterPageView(TemplateView):
-    template_name = "signup.html"
-
-class DashboardView(TemplateView):
-    template_name = "Dashboard.html"
-
-class DashboardSectionView(TemplateView):
-    template_name = "sectionlist.html"
-
-class DashboardSection3View(TemplateView):
-	template_name = "Section3.html"
-
-class DashboardSection4View(TemplateView):
-	template_name = "Section4.html"
-
-class DashboardSection5View(TemplateView):
-	template_name = "Section5.html"
+def dashboard_section(request):
+        return render('sectionlist.html')
 
 def section3(request, name):
-	f = open(os.path.abspath('site_ver1/RTAdocs/QT1_1'), 'r')
-	QT1_1=[]
-	for line in f:
-		QT1_1.append(line)
-	f.close()
-	f = open(os.path.abspath('site_ver1/RTAdocs/QT1_2'), 'r')
-	QT1_2=[]
-	for line in f:
-		QT1_2.append(line)
-	f.close()
-	QT1 = zip(QT1_1, QT1_2)
-	f = open(os.path.abspath('site_ver1/RTAdocs/QT2'), 'r')
-	QT2=[]
-	for line in f:
-		QT2.append(line)
-	f.close()
-	if request.method == 'POST':
-		form = CoverLetterForm(request.POST)
-		if form.is_valid():
-			p = FormSection3(project = "", cvl = form.cleaned_data["cvl"])
-			p.save()
-	else:
-		form = CoverLetterForm()
-	
-	return render(request,'Section3.html', {'form': form, 'QT1': QT1, 'QT2': QT2, 'name': name, 'section_name':"Section 3: RTA Checklist"})
+    f = open(os.path.abspath('site_ver1/RTAdocs/QT1_1'), 'r')
+    QT1_1=[]
+    for line in f:
+        QT1_1.append(line)
+    f.close()
+    f = open(os.path.abspath('site_ver1/RTAdocs/QT1_2'), 'r')
+    QT1_2=[]
+    for line in f:
+        QT1_2.append(line)
+    f.close()
+    QT1 = zip(QT1_1, QT1_2)
+    f = open(os.path.abspath('site_ver1/RTAdocs/QT2'), 'r')
+    QT2=[]
+    for line in f:
+        QT2.append(line)
+    f.close()
+    if request.method == 'POST':
+        form = CoverLetterForm(request.POST)
+        if form.is_valid():
+            p = FormSection3(project = "", cvl = form.cleaned_data["cvl"])
+            p.save()
+    else:
+        form = CoverLetterForm()
+
+    return render(request,'Section3.html', {'form': form, 'QT1': QT1, 'QT2': QT2, 'name': name, 'section_name':"Section 3: RTA Checklist"})
 
 def section4(request, name):
-	if request.method == 'POST':
-		form = Section4Form(request.POST)
-		if form.is_valid():
-			p = FormSection4(project = "", number = form.cleaned_data["number"], device_name= form.cleaned_data["device_name"], indication = form.cleaned_data["indication"])
-			p.save()
-	else:
-		form = Section4Form()
-	return render(request,'Section4.html', {'form': form,'name': name, 'section_name':"Section 4: Indications for Use Statement"})
+    # TODO: do we need to revalidate the session project id? - it's server side, so it should be fine?
+    project = Project.objects.filter(id      = request.session['project_id'],
+                                     creator = request.user).first()
+    if (project is None):
+        # if this is none, then the session's cached project_id isn't in any of the user's projects
+        # TODO: maybe include error string saying that the session expired or something
+        return redirect("/dashboard")
+
+    previous = FormSection4.objects.filter(project = project).first()
+
+    if request.method == 'POST':
+        form = Section4Form(request.POST)
+        if form.is_valid():
+            if (previous is not None):
+                p = previous
+            else:
+                p = FormSection4()
+
+            p.project     = project
+            p.number      = form.cleaned_data['number']
+            p.device_name = form.cleaned_data['device_name']
+            p.indication  = form.cleaned_data['indication']
+            p.save()
+
+    else:
+        if (previous is not None):
+            previous_data = {'number'       : previous.number,
+                             'device_name'  : previous.device_name,
+                             'indication'   : previous.indication}
+        else:
+            previous_data = None
+        form = Section4Form(initial = previous_data)
+
+    return render(request,'Section4.html', {'form': form,'name': name, 'section_name':"Section 4: Indications for Use Statement"})
 
 def section5(request, name):
-	if request.method == 'POST':
-		form = Section5Form(request.POST)
-		if form.is_valid():
-			p = FormSection5(project = "", summary = form.cleaned_data["summary"])
-			p.save()
-	else:
-		form = Section5Form()
-	return render(request,'Section5.html', {'form': form, 'name': name, 'section_name':"Section 5: 510K Summary / 510K Statement" })
+    if request.method == 'POST':
+        form = Section5Form(request.POST)
+        if form.is_valid():
+            p = FormSection5(project = "", summary = form.cleaned_data["summary"])
+            p.save()
+    else:
+       form = Section5Form()
+    return render(request,'Section5.html', {'form': form, 'name': name, 'section_name':"Section 5: 510K Summary / 510K Statement" })
 
 def whatever(request):
-	if request.method == "POST":
-		form = PersonForm(request.POST)
-		if form.is_valid():
-			u = User.objects.create_user(username = form.cleaned_data["email"], password = form.cleaned_data["password"])
-			u.person.role = form.cleaned_data["role"][0]
-			u.person.name = form.cleaned_data["name"]
-			u.save()
-			return HttpResponseRedirect("/loginpage")
-	else:
-		form = PersonForm()
-	return render(request, 'signup.html', {'form': form})
-		
+    if request.method == "POST":
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            u = User.objects.create_user(username = form.cleaned_data["email"], password = form.cleaned_data["password"])
+            u.person.role = form.cleaned_data["role"][0]
+            u.person.name = form.cleaned_data["name"]
+            u.save()
+            return HttpResponseRedirect("/loginpage")
+    else:
+        form = PersonForm()
+    return render(request, 'signup.html', {'form': form})
+
 def loginattempt(request):
-	if request.method == "POST":
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			user = authenticate(username=form.cleaned_data["email"], password = form.cleaned_data["password"])
-			if user is not None:
-				login(request, user)
-				return redirect("/dashboard")
-	else:
-		form = LoginForm()
-	return render(request, 'loginpage.html', {'form': form})
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data["email"], password = form.cleaned_data["password"])
+            if user is not None:
+                login(request, user)
+                return redirect("/dashboard")
+    else:
+        form = LoginForm()
+    return render(request, 'loginpage.html', {'form': form})
 
 def logouttry(request):
-	if request.user.is_authenticated:
-		logout(request)
-	return HttpResponseRedirect("/loginpage")
-#TODO: consider using django.contrib.auth.mixins.LoginRequiredMixin	
+    if request.user.is_authenticated:
+        logout(request)
+    return HttpResponseRedirect("/loginpage")
+
 @login_required
 def dashboard(request):
-	role = request.user.person.role
-	if role == 'R':
-		projects = Project.objects.filter(creator = request.user)
-	elif role == "C":
-		projects = Project.objects.filter(client = request.user)
-	data = []
-	for project in projects:
-		data.append({'name': project.proj_name})
-	return render(request, 'dashboard.html', {'projects': data, 'role': role == 'R'})
+    role = request.user.person.role
+    if role == 'R':
+        projects = Project.objects.filter(creator = request.user)
+    elif role == 'C':
+        projects = Project.objects.filter(client = request.user)
+    data = []
+    request.session['project_id'] = 0 # clearing the session id
+    for project in projects:
+            data.append({'name': project.proj_name, 'id' : project.id})
+    return render(request, 'dashboard.html', {'projects': data, 'role': role == 'R'})
 @login_required
 def newproject(request):
-	if request.method == "POST":
-		form = ProjectForm(request.POST)
-		if form.is_valid():
-			if form.cleaned_data["email_recipient"] != '':
-				clients = User.objects.filter(username = form.cleaned_data["email_recipient"])
-				for client in clients:
-					proj = Project(creator = request.user, proj_name = form.cleaned_data["proj_name"], business_name = form.cleaned_data["business_name"], client = client)
-				email = EmailMessage(proj.business_name +' - ' + proj.proj_name , 'A project has just been created for you by your Regulatory Consultant. Go to www.bluestream.com to get started', to=[form.cleaned_data["email_recipient"]])
-				email.send()
-			else:
-				proj = Project(creator = request.user, proj_name = form.cleaned_data["proj_name"], business_name = form.cleaned_data["business_name"])
-			proj.save();
-			return HttpResponseRedirect("/dashboard")
-	else:
-		form = ProjectForm()
-	return render(request, 'newproject.html', {'form': form})
-	
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            if form.cleaned_data["email_recipient"] != '':
+                clients = User.objects.filter(username = form.cleaned_data["email_recipient"])
+                for client in clients:
+                    proj = Project(creator = request.user, proj_name = form.cleaned_data["proj_name"], business_name = form.cleaned_data["business_name"], client = client)
+                email = EmailMessage(proj.business_name +' - ' + proj.proj_name , 'A project has just been created for you by your Regulatory Consultant. Go to www.bluestream.com to get started', to=[form.cleaned_data["email_recipient"]])
+                email.send()
+            else:
+                proj = Project(creator = request.user, proj_name = form.cleaned_data["proj_name"], business_name = form.cleaned_data["business_name"])
+            proj.save();
+            return HttpResponseRedirect("/dashboard")
+    else:
+        form = ProjectForm()
+    return render(request, 'newproject.html', {'form': form})
+
 @login_required
 def showproject(request, name):
-	role = request.user.person.role
-	if role == "R":
-		project = Project.objects.filter(proj_name = name, creator = request.user)
-	else:
-		project = Project.objects.filter(proj_name = name, client = request.user)
-	if project:
-		return render(request, 'sectionlist.html', {'name': name})
-	else:
-		return HttpResponseRedirect("/dashboard")
+    role = request.user.person.role
+    project_id = int(request.GET['id'])
+    if role == "R":
+        project = Project.objects.filter(id = project_id, creator = request.user)
+    else:
+        project = Project.objects.filter(id = project_id, client = request.user)
+    if project:
+        request.session['project_id'] = project_id
+        return render(request, 'sectionlist.html', {'name': name})
+    else:
+        return HttpResponseRedirect("/dashboard")
 
 def logout_view(request):
     logout(request)
 
-	
+
--- Bluestream/util/check_db.py
+++ Bluestream/util/check_db.py
@@ -1,19 +1,48 @@
 import sqlite3
 
+####### Usage:
+#           - modify TEST_NUMBERS
+#             to include indeces of select_strings
+#
+#           - add sqlite3 select statementsto select_strings
+#
+
+TEST_NUMBERS = [6]
+
 DB_PATH = '../db.sqlite3'
 
 conn = sqlite3.connect(DB_PATH)
 c = conn.cursor()
 
 # example:
-# print sqlite tables
-res = c.execute('select name from sqlite_master where type="table"').fetchall()
-print(res)
+select_strings = [
+        # (0) print sqlite tables
+        'select name from sqlite_master where type="table"',
 
-# print person database
-res = c.execute('select * from site_ver1_person').fetchall()
-print(res)
+        # (1) print person database
+        'select * from site_ver1_person',
 
-# print all from auth database
-res = c.execute('select * from auth_user').fetchall()
-print(res)
+        # (2) print all from auth database
+        'select * from auth_user',
+
+        # (3) print all from session database
+        'select * from django_session',
+
+        # (4) print all from section4 database
+        'select * from site_ver1_formsection4 where id>=3',
+
+        # (5) drop all values from section4 database
+        'delete from site_ver1_formsection4 where id>=1',
+
+        # (6) print all from section5 database
+        'select * from site_ver1_formsection5 where id>=3',
+        ]
+
+for i in TEST_NUMBERS:
+    res = c.execute(select_strings[i]).fetchall()
+    print(res)
+    print('\n')
+
+c.close()
+conn.commit()
+conn.close()
